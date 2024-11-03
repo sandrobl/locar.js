3a5a99db68388f736f66abb7681c0ba52dec5346
Nick Whitelegg <nickw4426@gmail.com>
2024-11-03 22:06:49 +0100

    Add click handler and two more examples

--- examples/vite.config.mjs
+++ examples/vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 const entries = { main: 'index.html' };
-['01-helloworld', '02-gps-and-sensors'].forEach ( example => {
+['01-helloworld', '02-gps-and-sensors', '03-ar-objects', '04-api-communication'].forEach ( example => {
     entries[example] = resolve(__dirname, `${example}/index.html`);
 });
 
